b0a5a9070f040aa1f67e1084edd07b075390acca
seongjineee <sungjin8506@naver.com>
2025-07-25 10:36:18 +0900

    곱셉 추가

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+//작업2
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("1+1=2");
+        System.out.println("1-1=0");
+        System.out.println("2*4=8");
+        System.out.println("4/2=2");
+        System.out.println("학원에서 작업1");
+        System.out.println("집에서 작업1");
+        System.out.println("학원에서 작업1");
+        System.out.println("집에서 작업2");
+    }
+}
